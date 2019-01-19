16c7e5ef981a69a75dbd92507ede9244cac9c21d
VentGrey <omarpurataf@gmail.com>
2019-01-19 13:36:41 -0600

    Updated numbers

--- src/code/rust/shadow.rs
+++ src/code/rust/shadow.rs
@@ -3,7 +3,11 @@ fn main() {
 
     let cow = cow + 1;
 
-    let cow = cow * 2;
+    let cow = cow - 3;
+
+    let cow = cow * 9;
+
+    let cow = cow / 2;
 
     printf("El valor de la vaca es: {}", cow);
 }
