df9b76026250a5962764aa04717fe587ead83dd6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-24 04:57:15 +0000

    Deploy VentGrey/rusty-learn to github.com/VentGrey/rusty-learn.git:gh-pages

---
+++ code/rust/shadow.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let cow = 20;
+
+    let cow = cow + 1;
+
+    let cow = cow - 3;
+
+    let cow = cow * 9;
+
+    let cow = cow / 2;
+
+    printf("El valor de la vaca es: {}", cow);
+}
